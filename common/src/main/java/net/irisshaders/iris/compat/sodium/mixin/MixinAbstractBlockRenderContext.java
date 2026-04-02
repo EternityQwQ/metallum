70522e51da5d5b7510c800f91cf031e10ff3613e
竹若泠 <98583550+ZhuRuoLing@users.noreply.github.com>
2026-04-03 03:52:13 +0800

    Make render layers in block.properties working in 26.1 (#3062)
    * Make render layers in block.properties working in 26.1
    
    * Switch to ModifyArg for better performance

--- common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinAbstractBlockRenderContext.java
+++ common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinAbstractBlockRenderContext.java
@@ -4,12 +4,12 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import net.caffeinemc.mods.sodium.client.render.model.AbstractBlockRenderContext;
 import net.irisshaders.iris.compat.general.IrisModSupport;
-import net.irisshaders.iris.platform.IrisPlatformHelpers;
+import net.irisshaders.iris.shaderpack.materialmap.BlockRenderType;
 import net.irisshaders.iris.shaderpack.materialmap.WorldRenderingSettings;
-import net.irisshaders.iris.vertices.BlockSensitiveBufferBuilder;
 import net.irisshaders.iris.vertices.sodium.terrain.VertexEncoderInterface;
 import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.client.renderer.block.dispatch.BlockStateModelPart;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,6 +18,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.caffeinemc.mods.sodium.client.render.model.MutableQuadViewImpl;
 
@@ -43,6 +44,34 @@ public class MixinAbstractBlockRenderContext {
 				((VertexEncoderInterface) r).overrideBlock(WorldRenderingSettings.INSTANCE.getBlockStateIds().getInt(override));
 			}
 		}
+	}
+
+	@ModifyArg(
+		method = "bufferDefaultModel",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/caffeinemc/mods/sodium/client/render/model/MutableQuadViewImpl;setRenderType(Lnet/minecraft/client/renderer/chunk/ChunkSectionLayer;)Lnet/caffeinemc/mods/sodium/client/render/model/MutableQuadViewImpl;"
+		)
+	)
+	private @Nullable ChunkSectionLayer handleShaderPackTransparency(
+		@Nullable ChunkSectionLayer renderLayer
+	) {
+		if (!((Object) this instanceof BlockRenderer) || WorldRenderingSettings.INSTANCE.getBlockTypeIds() == null) {
+			return renderLayer;
+		}
+		BlockState state = this.state;
+		if (state == null) {
+			return renderLayer;
+		}
+		BlockRenderType blockRenderType = WorldRenderingSettings.INSTANCE.getBlockTypeIds().get(state.getBlock());
+		if (blockRenderType == null) {
+			return renderLayer;
+		}
+		return switch (blockRenderType) {
+			case SOLID -> ChunkSectionLayer.SOLID;
+			case CUTOUT, CUTOUT_MIPPED -> ChunkSectionLayer.CUTOUT;
+			case TRANSLUCENT -> ChunkSectionLayer.TRANSLUCENT;
+		};
 	}
 
 	@Inject(method = "bufferDefaultModel", at = @At(value = "TAIL"))
